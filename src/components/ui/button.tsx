495c38011a19998cd0ce4b1c36ecdf096df70eaa
Stuart Muyambi <stuartmuyambi@gmail.com>
2026-01-08 10:00:54 +0300

    improve button apperance

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,10 +17,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 px-3",
-        lg: "h-11 px-8",
-        icon: "h-10 w-10",
+        default: "h-12 px-6 py-3",
+        sm: "h-10 px-4 text-xs",
+        lg: "h-14 px-8 text-base",
+        icon: "h-12 w-12",
       },
     },
     defaultVariants: {
